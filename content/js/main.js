19ccfc49788f5d7380ecf60bc1e1c18733d8e68c
islamnashaat03 <islamnashaat03@gmail.com>
2021-10-09 09:50:16 +0200

    add scroll top top button

--- content/js/main.js
+++ content/js/main.js
@@ -65,6 +65,25 @@ $(document).ready(function () {
       },
     },
   });
+
+   // START SCROLL TO TOP BUTTON
+   if (window.matchMedia('(min-width:992px)').matches){
+    let span = document.querySelector(".up");
+    window.onscroll = () => {
+      if (scrollY >= 800) {
+        span.classList.add("active");
+      } else {
+        span.classList.remove("active");
+      }
+    };
+    span.onclick = () => {
+      window.scrollTo({
+        top:0,
+        behavior:"smooth",
+      })
+    }
+  }
+  // END SCROLL TO TOP BUTTON
   if ($(".contact-us-page").length > 0) {
     $("#myform").validate({
       // onfocusout: false,
